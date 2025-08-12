163d1a356b4dc2ce433a5161202b930ad03d49e7
Gumilarmn <gumilarmn16@gmail.com>
2025-08-12 11:02:04 +0700

    fix

--- lib.php
+++ lib.php
@@ -95,7 +95,7 @@ function quizgenerator_create_question($categoryid, $questiondata)
             $answerobj->fraction = $answer['fraction'];
             $answerobj->feedback = '';
             $answerobj->feedbackformat = FORMAT_HTML;
-            $DB->insert_record('question_answers', $answerobj);
+            $DB->insert_record('Youtubes', $answerobj);
         }
     }
 
@@ -146,107 +146,30 @@ function quizgenerator_get_course_documents($courseid)
     return $allfiles;
 }
 
-// function quizgenerator_create_question($categoryid, $questiondata)
-// {
-//     global $DB, $USER;
+/**
+ * Adds a new instance of the quizgenerator activity.
+ *
+ * @param object $quizgenerator Object containing the new instance data.
+ * @return int The ID of the new record.
+ */
+function quizgenerator_add_instance($quizgenerator)
+{
+    global $DB;
+    $quizgenerator->timecreated = time();
+    $quizgenerator->timemodified = time();
+    $id = $DB->insert_record('quizgenerator', $quizgenerator);
+    return $id;
+}
 
-//     if (!$categoryid) {
-//         throw new moodle_exception('Invalid question category ID');
-//     }
-
-//     $question = new stdClass();
-//     $question->category               = $categoryid;
-//     $question->name                   = $questiondata->name;
-//     $question->questiontext           = $questiondata->text;
-//     $question->questiontextformat     = FORMAT_HTML;
-//     $question->generalfeedback        = '';              // Tambahkan general feedback
-//     $question->generalfeedbackformat  = FORMAT_HTML;     // Format general feedback
-//     $question->qtype                  = 'multichoice';
-//     $question->defaultmark            = 1;
-//     $question->penalty                = 0.3333333;       // Nilai penalty default (jika perlu)
-//     $question->penaltyformat          = FORMAT_HTML;     // Format penalty
-//     $question->createdby              = $USER->id;
-//     $question->modifiedby             = $USER->id;
-//     $question->stamp                  = make_unique_id_code();
-//     $question->version                = 1;
-
-//     // Insert soal ke tabel 'question'
-//     $questionid = $DB->insert_record('question', $question);
-//     if (!$questionid) {
-//         throw new moodle_exception('Failed to insert question');
-//     }
-
-//     // Simpan jawaban ke tabel 'question_answers'
-//     foreach ($questiondata->answers as $answer) {
-//         $answerobj = new stdClass();
-//         $answerobj->question = $questionid;
-//         $answerobj->answer = $answer['text'];
-//         $answerobj->fraction = $answer['fraction'];
-//         $answerobj->feedback = '';
-//         $answerobj->feedbackformat = FORMAT_HTML;
-//         $DB->insert_record('question_answers', $answerobj);
-//     }
-
-//     return $questionid;
-// }
-
-// defined('MOODLE_INTERNAL') || die();
-
-// require_once($CFG->dirroot . '/question/editlib.php');
-// require_once($CFG->dirroot . '/question/type/multichoice/questiontype.php');
-
-// /**
-//  * Mengambil kategori soal dari course.
-//  * Jika ada lebih dari satu, gunakan kategori pertama.
-//  */
-// function quizgenerator_get_question_category($courseid)
-// {
-//     global $DB, $CFG;
-//     $context = context_course::instance($courseid);
-//     $categories = $DB->get_records('question_categories', ['contextid' => $context->id]);
-//     if (!$categories) {
-//         require_once($CFG->dirroot . '/question/lib.php');
-//         question_make_default_categories($courseid);
-//         $categories = $DB->get_records('question_categories', ['contextid' => $context->id]);
-//     }
-//     if (!$categories) {
-//         return null;
-//     }
-//     $category = reset($categories);
-//     return $category->id;
-// }
-
-// /**
-//  * Membuat soal multichoice menggunakan data dari $fromform.
-//  */
-// function quizgenerator_create_question_fromform($fromform)
-// {
-//     global $USER;
-
-//     if (empty($fromform->category) || empty($fromform->qtype)) {
-//         throw new moodle_exception('Category or question type is missing in form data.');
-//     }
-//     if (empty($fromform->name) || empty($fromform->questiontext)) {
-//         throw new moodle_exception('Required question data is missing.');
-//     }
-//     if (empty($fromform->answer) || !is_array($fromform->answer)) {
-//         throw new moodle_exception('No answers provided.');
-//     }
-
-//     // Objek minimal $question.
-//     $question = new stdClass();
-//     $question->category  = $fromform->category;
-//     $question->qtype     = $fromform->qtype;
-//     $question->createdby = $USER->id;
-
-//     $qtype = question_bank::get_qtype($fromform->qtype);
-//     if (!$qtype) {
-//         throw new moodle_exception('Question type not found: ' . $fromform->qtype);
-//     }
-
-//     $result = $qtype->save_question($question, $fromform);
-//     if (!isset($result->id)) {
-//         throw new moodle_exception('Failed to insert question');
-//     }
-//     return $result->id;
-// }
+/**
+ * Deletes an instance of the quizgenerator activity.
+ *
+ * @param int $id The ID of the instance to delete.
+ * @return bool True if successful, false otherwise.
+ */
+function quizgenerator_delete_instance($id)
+{
+    global $DB;
+    $DB->delete_records('quizgenerator', ['id' => $id]);
+    return true;
+}
